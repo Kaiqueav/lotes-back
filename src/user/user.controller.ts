7a65fc5f2a0fd96c780a1352b5dee94c755e35e3
kaiqueav <kaiquecerq66@outlook.com>
2024-10-07 22:31:07 -0300

    feat:  create post user route   update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDTO } from './user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor( private readonly UserService: UserService){}
+    @Post()
+    create(@Body()users: UserDTO){
+        this.UserService.create(users);
+    }
+}
