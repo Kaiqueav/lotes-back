d5267a819aa68057434b61ec40f9ae27dc9fb401
kaiqueav <kaiquecerq66@outlook.com>
2024-10-08 21:09:01 -0300

    feat: create post route for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResponseDTO } from './auth.dto';
 
@@ -6,11 +6,13 @@ import { AuthResponseDTO } from './auth.dto';
 export class AuthController {
     constructor(private readonly authService: AuthService){}
 
+    
+    @HttpCode(HttpStatus.OK)
     @Post('login')
     signIn(
         @Body('username') username:string,
         @Body('password') password:string
     ):AuthResponseDTO {
-        return this.authService.signIn(username, password);
+        return this.authService.singIn(username, password);
     }
 }
